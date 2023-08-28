5b356541e2d425c05d15d72576bcb2463d1d6d91
amoloye <adedamolaadegoke2@gmail.com>
2023-08-28 10:12:44 +0300

    removed the @RequiredArgsContructor and added @Autowired as dependency injection for the RestTemplate class

--- src/main/java/com/example/chatgptbot/controller/CustomBotController.java
+++ src/main/java/com/example/chatgptbot/controller/CustomBotController.java
@@ -2,7 +2,7 @@ package com.example.chatgptbot.controller;
 
 import com.example.chatgptbot.dto.ChatGPTRequest;
 import com.example.chatgptbot.dto.ChatGptResponse;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/bot")
 public class CustomBotController {
@@ -21,6 +20,7 @@ public class CustomBotController {
     @Value("${openai.api.url}")
     private String apiURL;
 
+    @Autowired
     private RestTemplate template;
 
     @GetMapping("/chat")
